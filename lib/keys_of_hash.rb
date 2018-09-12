8236b1c240a185e2d2cf6e11815a75efb1cc6a53
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 18:42:33 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     returned_keys = []
-    arguments.each do |key,value|
+    .each do |key,value|
       if value != arguments
         returned_keys << arguments
       end
