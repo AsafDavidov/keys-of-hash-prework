a3adbdb51cf4531d41bcc1ed3c0a231b797b6030
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 18:44:00 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
     returned_keys = []
     .each do |key,value|
       if value != arguments
-        returned_keys << arguments
+        returned_keys << key
       end
     end
     returned_keys
